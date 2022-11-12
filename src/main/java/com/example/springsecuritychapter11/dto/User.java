9f37d544aac4afde3b9c4c156b80586a4be08a10
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-13 00:01:22 +0900

    feat: add User

---
+++ src/main/java/com/example/springsecuritychapter11/dto/User.java
@@ -0,0 +1,10 @@
+package com.example.springsecuritychapter11.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+    private String code;
+}
