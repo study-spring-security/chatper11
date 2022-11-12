f887edd431b35491c3ea01cca2147c28205d4774
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-12 23:59:41 +0900

    fix: HttpSecurity update

--- src/main/java/com/example/springsecuritychapter11/config/ProjectConfig.java
+++ src/main/java/com/example/springsecuritychapter11/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -15,11 +16,13 @@ public class ProjectConfig {
     }
 
     @Bean
-    public void configure(HttpSecurity http) throws Exception {
+    public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // 엔드포인트를 직접 실행할 수 있게 csrf 비활성화
 
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll(); // 인증 없이 모든 호출 허용
+
+        return http.build();
     }
 }
