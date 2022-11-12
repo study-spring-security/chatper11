ea79e14d9f0d3dc370786f8b146180c378eb09f6
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-12 20:27:41 +0900

    chore: 주석 추가

--- src/main/java/com/example/springsecuritychapter11/user/GenerateUtil.java
+++ src/main/java/com/example/springsecuritychapter11/user/GenerateUtil.java
@@ -15,11 +15,11 @@ public final class GenerateUtil {
         String code;
 
         try {
-            SecureRandom random = SecureRandom.getInstanceStrong();
+            SecureRandom random = SecureRandom.getInstanceStrong(); // 임의의 int 값을 생성하는 SecureRandom 의 인스턴스를 만든다.
 
-            int c = random.nextInt(9000) + 1000;
+            int c = random.nextInt(9000) + 1000; // 0~8,999 사이의 값을 생성하고 1,000을 더해서 1,000 ~ 9,999(4자리) 사이의 값을 얻는다.
 
-            code = String.valueOf(c);
+            code = String.valueOf(c); // int 를 String으로 변환하고 반환한다.
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Problem when generating the random code. ");
         }
--- src/main/java/com/example/springsecuritychapter11/user/UserService.java
+++ src/main/java/com/example/springsecuritychapter11/user/UserService.java
@@ -25,18 +25,21 @@ public class UserService {
         userRepository.save(user);
     }
 
+    /**
+     * 사용자가 인증되면 인증 서버는 사용자를 위한 OTP를 생성한다.
+     */
     public void auth(User user) {
-        Optional<User> o = userRepository.findUserByUsername(user.getUsername());
+        Optional<User> o = userRepository.findUserByUsername(user.getUsername()); // 데이터 베이스에서 사용자 검색
 
-        if (o.isPresent()) {
+        if (o.isPresent()) { // 사용자가 있으면 암호확인
             User u = o.get();
             if (passwordEncoder.matches(
                     user.getPassword(),
                     u.getPassword()
             )) {
-                renewOpt(u);
+                renewOpt(u); // 암호가 맞으면 새 OPT 생성
             } else {
-                throw new BadCredentialsException("Bad credentials.");
+                throw new BadCredentialsException("Bad credentials."); // 암호가 틀러기나 사용자가 없으면 예외 투척
             }
         } else {
             throw new BadCredentialsException("Bad credentials.");
@@ -44,29 +47,32 @@ public class UserService {
     }
 
     private void renewOpt(User user) {
-        String code = GenerateUtil.generateCode();
+        String code = GenerateUtil.generateCode(); // OTP를 위한 임의의 수 생성
 
-        Optional<Otp> userOtp = otpRepository.findOtpByUsername(user.getUsername());
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(user.getUsername()); // 사용자 이름으로 OTP 검색
 
         if (userOtp.isPresent()) {
             Otp otp = userOtp.get();
-            otp.setCode(code);
+            otp.setCode(code); // 이 사용자 이름에 대한 OTP가 있으면 값 업데이트
         } else {
             Otp otp = new Otp();
             otp.setUsername(user.getUsername());
             otp.setCode(code);
-            otpRepository.save(otp);
+            otpRepository.save(otp); // 이 사용자 이름에 대한 OTP 가 없으면 생성된 값으로 새 레코드 생성
         }
     }
 
+    /**
+     * Otp 검증 메서드
+     */
     public boolean check(Otp optToValidate) {
-        Optional<Otp> userOtp = otpRepository.findOtpByUsername(optToValidate.getUsername());
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(optToValidate.getUsername()); // 사용자 이름으로 Opt 검색
 
         if (userOtp.isPresent()) {
             Otp otp = userOtp.get();
 
-            return optToValidate.getCode().equals(otp.getCode());
+            return optToValidate.getCode().equals(otp.getCode()); // 데이터베이스에 OTP가 있고 비즈니스 논리 서버에서 받은 OTP와 일치하면 true 반환
         }
-        return false;
+        return false; // 그렇지 않으면 false 반환
     }
 }
