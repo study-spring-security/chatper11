b708f4a8ec820d0ca2311279081850d3ff76012d
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-12 19:44:47 +0900

    feat: add UserRepository

---
+++ src/main/java/com/example/springsecuritychapter11/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.springsecuritychapter11.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findUserByUsername(String username);
+}
