333ae5571c28c8588a697d201f10c35b4344fda9
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-13 00:00:02 +0900

    feat: add AuthController

---
+++ src/main/java/com/example/springsecuritychapter11/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.example.springsecuritychapter11.controller;
+
+import com.example.springsecuritychapter11.opt.Otp;
+import com.example.springsecuritychapter11.user.User;
+import com.example.springsecuritychapter11.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "Test";
+    }
+
+    @PostMapping("/user/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @PostMapping("/user/auth")
+    public void auth(@RequestBody User user) {
+        userService.auth(user);
+    }
+
+    @PostMapping("/opt/check")
+    public void check(@RequestBody Otp otp, HttpServletResponse response) {
+        if (userService.check(otp)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}
