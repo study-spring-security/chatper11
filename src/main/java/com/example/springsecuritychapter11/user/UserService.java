935c23b5319b2cbcc7e05e964e887ea99b84167f
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-12 20:10:30 +0900

    feat: add auth in UserService

--- src/main/java/com/example/springsecuritychapter11/user/UserService.java
+++ src/main/java/com/example/springsecuritychapter11/user/UserService.java
@@ -1,10 +1,14 @@
 package com.example.springsecuritychapter11.user;
 
+import com.example.springsecuritychapter11.opt.Otp;
 import com.example.springsecuritychapter11.opt.OtpRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,4 +25,38 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public void auth(User user) {
+        Optional<User> o = userRepository.findUserByUsername(user.getUsername());
+
+        if (o.isPresent()) {
+            User u = o.get();
+            if (passwordEncoder.matches(
+                    user.getPassword(),
+                    u.getPassword()
+            )) {
+                renewOpt(u);
+            } else {
+                throw new BadCredentialsException("Bad credentials.");
+            }
+        } else {
+            throw new BadCredentialsException("Bad credentials.");
+        }
+    }
+
+    private void renewOpt(User user) {
+        String code = GenerateUtil.generateCode();
+
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(user.getUsername());
+
+        if (userOtp.isPresent()) {
+            Otp otp = userOtp.get();
+            otp.setCode(code);
+        } else {
+            Otp otp = new Otp();
+            otp.setUsername(user.getUsername());
+            otp.setCode(code);
+            otpRepository.save(otp);
+        }
+
+    }
 }
