4bd59f924114d44f9341b897899a604b10218a85
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-13 02:32:10 +0900

    feat: add OptAuthenticationProvider

---
+++ src/main/java/com/example/springsecuritychapter11/config/OptAuthenticationProvider.java
@@ -0,0 +1,37 @@
+package com.example.springsecuritychapter11.config;
+
+import com.example.springsecuritychapter11.service.AuthenticationServiceProxy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OptAuthenticationProvider implements AuthenticationProvider {
+
+    private final AuthenticationServiceProxy proxy;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String code = String.valueOf(authentication.getCredentials());
+
+        boolean result = proxy.sendOTP(username, code);
+
+        if (result) {
+            return new OtpAuthentication(username, code);
+        } else {
+            throw new BadCredentialsException("Bad credentials.");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
