67ececb277bde808957bf6f7450620bfceee6de6
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-12 19:46:05 +0900

    feat: add OtpRepository

---
+++ src/main/java/com/example/springsecuritychapter11/opt/OtpRepository.java
@@ -0,0 +1,10 @@
+package com.example.springsecuritychapter11.opt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OtpRepository extends JpaRepository<Otp, String> {
+
+    Optional<Otp> findOtpByUsername(String username);
+}
